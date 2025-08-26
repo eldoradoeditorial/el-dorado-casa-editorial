1df749282e226dffe62ffc48e1e38e034c15ef6d
eldoradoeditorial <email@eldoradoeditorial.com>
2025-08-25 22:24:51 -0500

    Last touches

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,16 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://splendorous-valkyrie-2fcaf1.netlify.app',
+  site: "https://splendorous-valkyrie-2fcaf1.netlify.app",
   integrations: [
     react(),
-    tailwind({
-      applyBaseStyles: false
-    }),
+    tailwind({ applyBaseStyles: false }),
     sitemap(),
   ],
-  vite: {
-    ssr: {
-      noExternal: ['@sanity/client', '@sanity/image-url']
-    }
-  },
   build: {
-    inlineStylesheets: 'auto'
+    inlineStylesheets: "auto",
   },
-  server: {
-    port: 3000,
-    host: true
-  }
 });
