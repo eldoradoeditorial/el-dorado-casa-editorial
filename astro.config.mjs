98a14a438c39a2704f9ac18be9deb560928adb8c
eldoradoeditorial <email@eldoradoeditorial.com>
2025-08-26 16:59:02 -0500

    Setting metadata for engine search

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://splendorous-valkyrie-2fcaf1.netlify.app",
+  site: "https://eldoradoeditorial.com",
   integrations: [
     react(),
     tailwind({ applyBaseStyles: false }),
