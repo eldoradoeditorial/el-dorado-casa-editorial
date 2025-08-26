3b4f8218cc86628a9c9ce4c352b763a08a0fa7bc
eldoradoeditorial <email@eldoradoeditorial.com>
2025-08-25 22:06:03 -0500

    Add correct Netlify site URL to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://splendorous-valkyrie-2fcaf1.netlify.app',
   integrations: [
     react(),
     tailwind({
